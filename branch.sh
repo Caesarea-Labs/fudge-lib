b8d4a517a36848244073ce9f283d87fddd359081
natanfudge <natan.lifsiz@gmail.com>
2023-10-16 10:45:01 +0300

    this should be more correct

--- branch.sh
+++ branch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ### Branches the parent project and the submodule
 
-# 1. Set submodule branch in .gitmodules
-git submodule set-branch --branch $1 -- client/src/fudge-lib
-# 2. Set branch of git repository of submodule
-git submodule foreach git checkout -b $1
-# 3. Set branch of parent repository
+# 1. Set branch of parent repository
 git checkout -b $1
+# 2. Set submodule branch in .gitmodules
+git submodule set-branch --branch $1 -- client/src/fudge-lib
+# 3. Set branch of git repository of submodule
+git submodule foreach git checkout -b $1
--- checkout.sh
+++ checkout.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ### Checks out a parent project and submodule branch
 
-# 1. Set submodule branch in .gitmodules
-git submodule set-branch --branch $1 -- client/src/fudge-lib
-# 2. Set branch of git repository of submodule
-git submodule foreach git checkout $1
-# 3. Set branch of parent repository
+# 1. Set branch of parent repository
 git checkout $1
+## 2. Set submodule branch in .gitmodules
+#git submodule set-branch --branch $1 -- client/src/fudge-lib
+## 3. Set branch of git repository of submodule
+#git submodule foreach git checkout $1
