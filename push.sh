eb2df7af9042f8d3ea964ffcd8d660adf3eb1432
natanfudge <natan.lifsiz@gmail.com>
2023-10-16 10:31:20 +0300

    test

--- push.sh
+++ push.sh
@@ -2,8 +2,7 @@
 # 1. Update submodule
 git submodule foreach git add .
 git submodule foreach git commit -m "$1"
-#It will always push to main which is not optimal but works for now
-#git submodule foreach git push origin HEAD:main
+git submodule foreach git push
 # 2. Update reference to submodule so that git uses the latest version
 git submodule update --remote client/src/fudge-lib
 # 3. Update this project, including new reference to submodule
