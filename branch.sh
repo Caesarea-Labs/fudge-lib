603d25a367b2e68ec76b61a429d7a40f9cd9cb60
natanfudge <natan.lifsiz@gmail.com>
2023-10-16 10:39:29 +0300

    lets see

---
+++ branch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 1. Set submodule branch in .gitmodules
+git submodule set-branch --branch $1 -- client/src/fudge-lib
+# 2. Set branch of git repository of submodule
+git submodule foreach git checkout -b $1
+# 3. Set branch of parent repository
+git checkout -b $1
