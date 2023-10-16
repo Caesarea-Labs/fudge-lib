3d028dfac697c490b3bd8bd225c6b2b1615fdeea
natanfudge <natan.lifsiz@gmail.com>
2023-10-16 10:49:21 +0300

    final test for sure

--- checkout.sh
+++ checkout.sh
@@ -3,7 +3,8 @@
 
 # 1. Set branch of parent repository
 git checkout $1
+## 2. Set branch of git repository of submodule
+git submodule foreach git checkout $1
 ## 2. Set submodule branch in .gitmodules
 #git submodule set-branch --branch $1 -- client/src/fudge-lib
-## 3. Set branch of git repository of submodule
-#git submodule foreach git checkout $1
+
